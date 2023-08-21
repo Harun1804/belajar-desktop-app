4e74a9b4643326723e98d0421c8e2dff24313fd7
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-08-21 17:42:18 +0700

    Variable

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World");
+            string characterName = "Harun";
+            int age = 35;
+
+            Console.WriteLine($"There one was a man named {characterName}");
+            Console.WriteLine($"He was {age} years old ");
+            Console.WriteLine($"He really liked the name {characterName}");
+            Console.WriteLine($"But didn't like being {age}");
+
+            Console.ReadLine();
         }
     }
 }

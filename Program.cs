f4f6a09ee7f5304934aabd50d75a35a06410810b
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-08-22 02:47:26 +0700

    Array

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
-﻿string color, pluralNoun, celebrity;
+﻿int[] numbers = { 1,2,3,4,5,6 };
+numbers[2] = 19;
+string[] friends = new string[2];
+friends[0] = "Aegis";
+friends[1] = "Luna";
 
-Console.Write("What Color Do You Want : ");
-color = Console.ReadLine();
-
-Console.Write("What Plural Noun Do You Want : ");
-pluralNoun = Console.ReadLine();
-
-Console.Write("What Celebrity Do You Want : ");
-celebrity = Console.ReadLine();
-
-Console.WriteLine("Roses are {0}", color);
-Console.WriteLine("{0} are blue", pluralNoun);
-Console.WriteLine("I Love {0}", celebrity);
-
-Console.ReadLine();
+Console.WriteLine(friends[0]);

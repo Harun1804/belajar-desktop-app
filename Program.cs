b4d34a2c0a22e9eb8e9ed76424c4a5b1c2a8d54a
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-08-21 18:38:35 +0700

    Working With String

--- Program.cs
+++ Program.cs
@@ -4,11 +4,8 @@
     {
         static void Main(string[] args) 
         {
-            string phrase = "Giraffe Academy";
-            char grade = 'A';
-            int age = 24;
-            double gpa = 3.2;
-            bool isMale = true;
+            string phrase = "Racoon academy";
+            Console.WriteLine(phrase.Substring(8, 2));
 
             Console.ReadLine();
         }

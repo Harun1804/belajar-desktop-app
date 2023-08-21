db323986e18f127f96c341271470388746d12166
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-08-22 02:30:05 +0700

    simple calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine("Hello, {0}", name);
+﻿Console.Write("Input First Number: ");
+int num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Second Number: ");
+int num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(num1 + num2);
+
 Console.ReadLine();
